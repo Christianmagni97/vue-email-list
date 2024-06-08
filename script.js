d7efa703a156e766386f3e5e464aa789f311e749
Christianmagni97 <christianmagni28@gmail.com>
2024-06-08 14:02:03 +0200

    aggiunto ciclo for in js e html

---
+++ script.js
@@ -0,0 +1,20 @@
+const { createApp } = vue 
+createApp({
+    data() {
+        return{
+            emailList : [],
+        }
+    }
+    methods: {
+        getData(){
+          for (let index = 0; index < 10; index+) { axios.get( 'https://flynn.boolean.careers/exercises/api/random/mail').then
+          ((response) => { 
+              console. log(response.data.response);
+              this.emailList.push(response.data.response);
+          });
+        }  
+    }  
+created(){
+    this.getData();
+    },
+}).mount ('#app');
